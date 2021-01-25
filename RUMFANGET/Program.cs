9a1904140845e807ea072a3c7df6a3c63270c0d1
Oliver Tobias Jensen <Olivertobias@live.dk>
2021-01-25 11:11:42 +0100

    Add project files.

---
+++ RUMFANGET/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RUMFANGET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Intast Længde");
+            double længde = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Intast Brede");
+            double brede = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Intast Højde");
+            double højde = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Rumfanget er " + længde * brede * højde);
+            Console.ReadLine();
+        }
+    }
+}
